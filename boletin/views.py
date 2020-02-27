3dc7d8252e61cc9de6f4b4edaf02e471a20a0066
J.L. Álvarez <alvarez@uhu.es>
2020-02-27 22:17:09 +0000

    version 0.3

--- boletin/views.py
+++ boletin/views.py
@@ -5,7 +5,13 @@ from django.shortcuts import render
 from .forms import SingupForm
 
 def inicio(request):
-    form = SingupForm()
+    form = SingupForm(request.POST or None)
+    print(dir(form))
+    if form.is_valid():
+        form_data = form.cleaned_data
+        print(form_data.get("nombre"))
+        print(form_data.get("email"))
+        
     contexto = {
         "form": form,
     }
